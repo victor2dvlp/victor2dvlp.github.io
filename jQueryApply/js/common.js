ce8601e5fcf7b423f37e2965246bd9784c0a858b
victor2dvlp <victor_sp@ukr.net>
2017-11-06 17:45:06 +0200

    Historical occasion! Aded js and maaaanyyy animations!

--- jQueryApply/js/common.js
+++ jQueryApply/js/common.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
 	var progress_circle_2 = $('.my-progress-bar-2');
 	var progress_circle_3 = $('.my-progress-bar-3');
 	var progress_circle_4 = $('.my-progress-bar-4');
-	var i = 0;
+	var i1 = 0, i2 = 0, i3 = 0, i4 = 0;
 	function myStart(){
 		
 	}
 	
 	$(window).scroll(function(){
-		if( $('.my-progress-bar-1')[0].getBoundingClientRect().top < 700 && i == 0){
+		if( $('.my-progress-bar-1')[0].getBoundingClientRect().top < 600 && i1 == 0){
 			progress_circle_1.circularProgress({
 								height: "200px",
 								width: "200px",
@@ -22,9 +22,10 @@ $(document).ready(function(){
 								percent: 2,
 								percentage: true
 							}).circularProgress('animate', 77, 2000);
+			i1 = 1;
 		
 		}
-		if( $('.my-progress-bar-2')[0].getBoundingClientRect().top < 700 && i == 0){
+		if( $('.my-progress-bar-2')[0].getBoundingClientRect().top < 600 && i2 == 0){
 			progress_circle_2.circularProgress({
 								height: "200px",
 								width: "200px",
@@ -35,9 +36,10 @@ $(document).ready(function(){
 								percent: 2,
 								percentage: true
 							}).circularProgress('animate', 96, 3000);
+			i2 = 1;
 
 		}
-		if( $('.my-progress-bar-3')[0].getBoundingClientRect().top < 700 && i == 0){
+		if( $('.my-progress-bar-3')[0].getBoundingClientRect().top < 600 && i3 == 0){
 			progress_circle_3.circularProgress({
 								height: "200px",
 								width: "200px",
@@ -48,9 +50,10 @@ $(document).ready(function(){
 								percent: 2,
 								percentage: true
 							}).circularProgress('animate', 31, 1000);
+			i3 = 1;
 
 		}
-		if( $('.my-progress-bar-4')[0].getBoundingClientRect().top < 700 && i == 0){
+		if( $('.my-progress-bar-4')[0].getBoundingClientRect().top < 600 && i4 == 0){
 			progress_circle_4.circularProgress({
 								height: "200px",
 								width: "200px",
@@ -61,7 +64,7 @@ $(document).ready(function(){
 								percent: 2,
 								percentage: true
 							}).circularProgress('animate', 54, 2000);
-			i = 1;
+			i4 = 1;
 		}
 	});
 });
